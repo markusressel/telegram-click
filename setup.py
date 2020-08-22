050a8e83433b316e2697f19b0fd5111d3a41c7e1
Markus Ressel <mail@markusressel.de>
2020-08-22 08:56:31 +0200

    v4.1.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ import subprocess
 
 from setuptools import setup, find_packages
 
-VERSION_NUMBER = "4.0.0"
+VERSION_NUMBER = "4.1.0"
 
 GIT_BRANCH = subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"])
 GIT_BRANCH = GIT_BRANCH.decode()  # convert to standard string
