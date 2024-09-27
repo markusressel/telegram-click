90268ffefd96ff084510cfb720f9ca7a578ab2c1
Markus Ressel <mail@markusressel.de>
2024-09-27 22:13:59 +0200

    simplify setup.py

--- setup.py
+++ setup.py
@@ -18,35 +18,13 @@
 #  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #  SOFTWARE.
 #
-import os
-import subprocess
 
 from setuptools import setup, find_packages
 
 VERSION_NUMBER = "6.0.3"
 
-GIT_BRANCH = subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"])
-GIT_BRANCH = GIT_BRANCH.decode()  # convert to standard string
-GIT_BRANCH = GIT_BRANCH.rstrip()  # remove unnecessary whitespace
-
-if GIT_BRANCH == "master":
-    DEVELOPMENT_STATUS = "Development Status :: 5 - Production/Stable"
-    VERSION_NAME = VERSION_NUMBER
-elif GIT_BRANCH == "beta":
-    DEVELOPMENT_STATUS = "Development Status :: 4 - Beta"
-    VERSION_NAME = "%s-beta" % VERSION_NUMBER
-elif GIT_BRANCH == "dev":
-    DEVELOPMENT_STATUS = "Development Status :: 3 - Alpha"
-    VERSION_NAME = "%s-dev" % VERSION_NUMBER
-elif os.environ.get("TRAVIS_BRANCH", None) == os.environ.get("TRAVIS_TAG", None) == "v{}".format(VERSION_NUMBER) or \
-        "GITHUB_RELEASE" in os.environ:
-    # travis tagged release branch
-    DEVELOPMENT_STATUS = "Development Status :: 5 - Production/Stable"
-    VERSION_NAME = VERSION_NUMBER
-else:
-    print("Unknown git branch, using pre-alpha as default")
-    DEVELOPMENT_STATUS = "Development Status :: 2 - Pre-Alpha"
-    VERSION_NAME = "%s-%s" % (VERSION_NUMBER, GIT_BRANCH)
+DEVELOPMENT_STATUS = "Development Status :: 5 - Production/Stable"
+VERSION_NAME = VERSION_NUMBER
 
 
 def readme_type() -> str:
